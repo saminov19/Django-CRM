c12c879f68ea2b524a94b7909a1d6aa5d592596e
Sultan Aminov <saminov19@gmail.com>
2023-04-02 17:01:49 +0200

    initial commit

---
+++ mydb.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+dataBase=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='Astana2025!',
+)
+
+
+#prepare cursor object
+cursorObject=dataBase.cursor()
+
+
+#create a database
+cursorObject.execute("CREATE DATABASE elderco")
+
+
+print("All done!")
