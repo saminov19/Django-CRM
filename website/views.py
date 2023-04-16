dd3c066e7bfce99092927879748b5a65aadb4768
Sultan Aminov <saminov19@gmail.com>
2023-04-17 00:59:37 +0200

    added individual record page

--- website/views.py
+++ website/views.py
@@ -52,3 +52,15 @@ def register_user(request):
         return render(request,'register.html',{'form':form})
     
     return render(request,'register.html',{'form':form})
+
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # look up records
+        customer_record=Record.objects.get(id=pk)
+        return render(request,'record.html',{'customer_record':customer_record})
+    else:
+        messages.success(request, "You must be logged in to view this page!")
+        return redirect('home')
+        
