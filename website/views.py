8e65422bdc70cd6f14f550ff744db072958a0e1f
Sultan Aminov <saminov19@gmail.com>
2023-04-17 23:37:25 +0200

    added delete record function

--- website/views.py
+++ website/views.py
@@ -64,3 +64,14 @@ def customer_record(request, pk):
         messages.success(request, "You must be logged in to view this page!")
         return redirect('home')
         
+        
+        
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it=Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record deleted successfully!")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to delete this record!")
+        return redirect('home')
